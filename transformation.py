6455628923092ea565ae4d3dd0529091a8e89d08
Adedola_ <abowabat@gmail.com>
2025-07-18 23:06:32 +0100

    Corrected transformation codes

--- transformation.py
+++ transformation.py
@@ -9,16 +9,26 @@ logging.basicConfig(filename='Api.log',
     format='%(asctime)s - %(levelname)s - %(message)s')
 
 
-def query_database():
+def query_database(cleaned_data):
+    """
+    Connects to the database and returns data from the 'cleaned_data' table
+    
+    Params:
+    cleaned_data (DataFrame): Unused, kept for compatibility
+
+    Returns:
+    DataFrame with queried data
+    """
     DB_HOST = os.getenv("DB_HOST")
     DB_NAME = os.getenv("DB_NAME")
     
     try:
         engine = create_engine(
             f"mssql+pyodbc://{DB_HOST}/{DB_NAME}?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes")
-        query = "SELECT * FROM "
+        query = "SELECT * FROM cleaned_data"
         df = pd.read_sql(query, con = engine)
-        logging.info("Read data from database successfully")
+        logging.info("Reading data from database")
+        return df
 
     except Exception as e:
         logging.error(f"Error reading data from database: {e}")
@@ -26,6 +36,17 @@ def query_database():
 
 
 def transform_data(df):
+    """
+    Transforms the input DataFrame by engineering new features related to transactions
+
+    It creates:
+    - 'TransactionType': categorizes amounts as 'Debit' or 'Credit'
+    - 'TransactionYear': extracts the year from the transaction date
+    - 'AmountCategory': classifies the amount into defined debit or credit categories
+
+    Returns:
+    Transformed DataFrame with the new columns added
+    """
     transform = df
     transform["TransactionType"] = transform["Amount"].where(transform["Amount"]<0, "Debit", "Credit")
     transform["TransactionYear"] = transform["TransactionDate"].dt.year.astype("Int64")
@@ -39,8 +60,7 @@ def transform_data(df):
     "High Debit Transaction",
     "Medium Debit Transaction",
     "Low Credit Transaction",
-    "Medium Credit Transaction"
-]
+    "Medium Credit Transaction"]
 
     transform["AmountCategory"] = np.select(conditions, choices, default="High Credit Transaction")
 
@@ -49,9 +69,10 @@ def transform_data(df):
 
 def load_to_db(data):
     """
-      Connects to a SQL Server database using credentials stored in environment variables.
-      Uses the ODBC driver to establish the connection and exports the cleaned country data
-      to specified database
+    Loads the final transaction data into the SQL Server database
+
+    Connects to the database and writes the DataFrame to the 'cleaned_data' table.
+    Replaces the table if it already exists
 
     """
 
@@ -64,7 +85,7 @@ def load_to_db(data):
          
         engine = create_engine(f"mssql+pyodbc://{DB_HOST}/{DB_NAME}?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes")
 
-        transformed_data.to_sql(name='country_data', con=engine ,if_exists='replace', index=False)
+        transformed_data.to_sql(name='cleaned_data', con=engine ,if_exists='replace', index=False)
         logging.info("Transformed data exported to database successfully")
 
     except Exception as e:
